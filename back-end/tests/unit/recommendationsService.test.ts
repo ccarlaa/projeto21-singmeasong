683cad65744fe192e394f7d828d9259b87bfc273
Carla A. C. Ribeiro <carlacarlaclementino@gmail.com>
2022-07-29 11:41:56 -0300

    Unit test of downvote function

--- back-end/tests/unit/recommendationsService.test.ts
+++ back-end/tests/unit/recommendationsService.test.ts
@@ -1,20 +1,16 @@
 import { faker } from '@faker-js/faker';
 import { jest } from '@jest/globals'
 
-import { recommendationRepository } from "../../src/repositories/recommendationRepository";
+import { recommendationRepository } from "../../src/repositories/recommendationRepository.js";
 import recommendationService from "../../src/services/recommendationsService.js"
 import { conflictError, notFoundError } from "../../src/utils/errorUtils.js"
 
 describe('Recommendation service test',() => {
     it("Should create a new recommendation", async() => {
-        jest.spyOn(recommendationRepository, 'findByName').mockImplementationOnce(() => {
-            return null
-        });
-        const createRecommendation = jest.spyOn(recommendationRepository, 'create').mockImplementationOnce(() => {
-            return null
-        });
+        jest.spyOn(recommendationRepository, 'findByName').mockImplementationOnce(null)
+        const createRecommendation = jest.spyOn(recommendationRepository, 'create').mockImplementationOnce(null);
 
-        const recommendation = returnRecomendation()
+        const recommendation = returnRecomendation(0)
         delete recommendation.id
         delete recommendation.score
 
@@ -24,7 +20,7 @@ describe('Recommendation service test',() => {
       })
 
       it("Should return conflict error for a recommendation already in use", async() => {
-        const recommendation = returnRecomendation()
+        const recommendation = returnRecomendation(0)
 
         jest.spyOn(recommendationRepository, 'findByName').mockResolvedValueOnce(recommendation);
       
@@ -32,7 +28,7 @@ describe('Recommendation service test',() => {
       })
 
       it("Should update vote (+1)", async() => {
-        const recommendation = returnRecomendation()
+        const recommendation = returnRecomendation(0)
 
         jest.spyOn(recommendationRepository, 'find').mockResolvedValueOnce(recommendation);
         const updatedRecommendation = jest.spyOn(recommendationRepository, 'updateScore').mockImplementationOnce(() => {
@@ -44,19 +40,46 @@ describe('Recommendation service test',() => {
         expect(updatedRecommendation).toHaveBeenCalled()
       })
 
-      it("Should return error for update vote (+1)", async() => {
+      it("Should return error for update vote -> recommendation not found", async() => {
         jest.spyOn(recommendationRepository, 'find').mockResolvedValueOnce(undefined);
 
         expect(recommendationService.upvote(1)).rejects.toEqual(notFoundError())
       })
+
+      it("Should update vote for score > -5 (-1)", async() => {
+        const recommendation = returnRecomendation(10)
+
+        jest.spyOn(recommendationRepository, 'find').mockResolvedValueOnce(recommendation);
+        const updatedRecommendation = jest.spyOn(recommendationRepository, 'updateScore').mockResolvedValueOnce(recommendation);
+        const removeRecommendation = jest.spyOn(recommendationRepository, 'remove').mockResolvedValueOnce(null);
+
+        await recommendationService.downvote(recommendation.id)
+      
+        expect(updatedRecommendation).toHaveBeenCalled()
+        expect(removeRecommendation).not.toHaveBeenCalled()
+      })
+
+      it("Should update vote for score < -5 (-1)", async() => {
+        const recommendation = returnRecomendation(-10)
+
+        jest.spyOn(recommendationRepository, 'find').mockResolvedValueOnce(recommendation);
+        const updatedRecommendation = jest.spyOn(recommendationRepository, 'updateScore').mockResolvedValueOnce(recommendation);
+        const removeRecommendation = jest.spyOn(recommendationRepository, 'remove').mockResolvedValueOnce(null);
+
+        await recommendationService.downvote(recommendation.id)
+      
+        expect(updatedRecommendation).toHaveBeenCalled()
+        expect(removeRecommendation).toHaveBeenCalled()
+      })
+
   });
 
-function returnRecomendation() {
+function returnRecomendation(score: number) {
     const recommendation = {
         id: 1,
         name: faker.music.songName(),
         youtubeLink: faker.internet.url(),
-        score: 0
+        score: score
     }
     return recommendation
 }
