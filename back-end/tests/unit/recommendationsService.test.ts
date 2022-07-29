78f3ac05426fbc89828196b14e4dbc9267ae0d89
Carla A. C. Ribeiro <carlacarlaclementino@gmail.com>
2022-07-29 10:06:10 -0300

    Unit test of upvote function

--- back-end/tests/unit/recommendationsService.test.ts
+++ back-end/tests/unit/recommendationsService.test.ts
@@ -3,7 +3,7 @@ import { jest } from '@jest/globals'
 
 import { recommendationRepository } from "../../src/repositories/recommendationRepository";
 import recommendationService from "../../src/services/recommendationsService.js"
-import { conflictError } from "../../src/utils/errorUtils.js"
+import { conflictError, notFoundError } from "../../src/utils/errorUtils.js"
 
 describe('Recommendation service test',() => {
     it("Should create a new recommendation", async() => {
@@ -11,31 +11,52 @@ describe('Recommendation service test',() => {
             return null
         });
         const createRecommendation = jest.spyOn(recommendationRepository, 'create').mockImplementationOnce(() => {
-            return undefined
+            return null
         });
 
-        const recommendation = {
-            name: faker.music.songName(),
-            youtubeLink: faker.internet.url()
-        }
-        
+        const recommendation = returnRecomendation()
+        delete recommendation.id
+        delete recommendation.score
+
         await recommendationService.insert(recommendation)
       
         expect(createRecommendation).toHaveBeenCalled()
       })
 
-      it("Should return conflict error", async() => {
-        const recommendation = {
-            id: 1,
-            name: faker.music.songName(),
-            youtubeLink: faker.internet.url(),
-            score: 0
-        }
+      it("Should return conflict error for a recommendation already in use", async() => {
+        const recommendation = returnRecomendation()
 
         jest.spyOn(recommendationRepository, 'findByName').mockResolvedValueOnce(recommendation);
-        
       
         expect(recommendationService.insert(recommendation)).rejects.toEqual(conflictError("Recommendations names must be unique"))
       })
+
+      it("Should update vote (+1)", async() => {
+        const recommendation = returnRecomendation()
+
+        jest.spyOn(recommendationRepository, 'find').mockResolvedValueOnce(recommendation);
+        const updatedRecommendation = jest.spyOn(recommendationRepository, 'updateScore').mockImplementationOnce(() => {
+            return null
+        });
+
+        await recommendationService.upvote(recommendation.id)
+      
+        expect(updatedRecommendation).toHaveBeenCalled()
+      })
+
+      it("Should return error for update vote (+1)", async() => {
+        jest.spyOn(recommendationRepository, 'find').mockResolvedValueOnce(undefined);
+
+        expect(recommendationService.upvote(1)).rejects.toEqual(notFoundError())
+      })
   });
 
+function returnRecomendation() {
+    const recommendation = {
+        id: 1,
+        name: faker.music.songName(),
+        youtubeLink: faker.internet.url(),
+        score: 0
+    }
+    return recommendation
+}
